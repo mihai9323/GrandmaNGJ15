59456a12b070b65d6a3658b3780d870b6ed52695
Anders <lunduz@gmail.com>
2015-02-07 18:48:11 +0100

    Types for obstacles

--- Assets/Scripts/Obstacles.cs
+++ Assets/Scripts/Obstacles.cs
@@ -10,7 +10,18 @@ public class Obstacles : MonoBehaviour {
         beds
         
     }
-    public ObstacleType type;
+
+    //private ObstacleType _type;
+    public ObstacleType type; 
+//{
+
+//        get {
+//            return _type;
+//        }
+//        set {
+//            _type = value;
+//        }
+//    }
 
 	// Use this for initialization
 	void Start () {
@@ -43,5 +54,25 @@ public class Obstacles : MonoBehaviour {
 
     void StationaryBehaviour() {
 
+    }
+
+    public void SetObstacleType(int typeWeight) {
+        Debug.Log("TypeWeight " + typeWeight);
+        if (typeWeight <= 25) {
+            type = ObstacleType.doctor;
+            renderer.material.color = Color.red;
+        }
+        else if (typeWeight > 25 && typeWeight <= 50) {
+            type = ObstacleType.nurse;
+            renderer.material.color = Color.blue;
+        }
+        else if (typeWeight > 50 && typeWeight <= 75) {
+            type = ObstacleType.beds;
+            renderer.material.color = Color.green;
+        }
+        else if (typeWeight > 75 && typeWeight <= 100) {
+            type = ObstacleType.tables;
+            renderer.material.color = Color.yellow;
+        }
     }
 }
