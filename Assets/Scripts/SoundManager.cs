08e99ad496b0a300122253f9ff6cce6f006a52fb
mihai9323 <mihai9323@gmail.com>
2015-02-08 00:04:58 +0100

    Punching and pills

---
+++ Assets/Scripts/SoundManager.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SoundManager : MonoBehaviour {
+	public static SoundManager s_Instance;
+	
+	
+	public AudioClip[] pillSounds;
+	public AudioClip[] punchSounds;
+	
+	public static AudioClip returnRandomSound(AudioClip[] soundsArray){
+		if(soundsArray != null && soundsArray.Length>0){
+			return soundsArray[Random.Range(0,soundsArray.Length)];
+		}else return null;
+	}
+}
