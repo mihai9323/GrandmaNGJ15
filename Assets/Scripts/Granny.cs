bc4290c18d34b12ea3a4cf2106e05e4cab1e5adc
Carsten Jørgensen <to.bogstaver@gmail.com>
2015-02-08 00:30:13 +0100

    Added HandsUpcheck

--- Assets/Scripts/Granny.cs
+++ Assets/Scripts/Granny.cs
@@ -10,6 +10,8 @@ public class Granny : MonoBehaviour {
 	private float timer;
 	private float power;
 	private int prevDirection;
+	private static int handUp=0;
+
 	private void Awake(){
 		s_Instance = this;
 		timer = -1;
@@ -47,9 +49,16 @@ public class Granny : MonoBehaviour {
 			s_Instance.timer = Time.time;
 		}
 		
-		
+
 		
 	}
+
+	public static void HandsUp (int hand, bool up){
+		if (up && hand!=handUp) {
+			//AudioSource.PlayClipAtPoint(
+		}
+	}
+
 	private void Update(){
 		if(Time.time>timer + .2f && timer!= -1){
 			Move (prevDirection, power);
