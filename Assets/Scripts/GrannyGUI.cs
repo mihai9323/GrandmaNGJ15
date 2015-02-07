73d952568d91ce9bf19f77acb3d9b12e6df744dc
Anders Jepsen <anders.jepsen@designit.com>
2015-02-07 12:54:44 +0100

    I just want the new stuff

---
+++ Assets/Scripts/GrannyGUI.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GrannyGUI : VRGUI 
+{
+	public Texture background;
+	public Texture slider;
+
+	public override void OnVRGUI()
+	{
+		GUILayout.BeginArea(new Rect(0, 0, Screen.width, Screen.height));
+
+		GUI.Box(new Rect((Screen.width/100)*20,(Screen.height/100)*30,(Screen.width/100)*60,50),"");
+		GUILayout.EndArea();
+	}
+}
