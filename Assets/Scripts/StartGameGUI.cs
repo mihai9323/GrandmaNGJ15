4d2420100c0a1ef05db5fbaffd1418f6d787afc0
Anders Jepsen <anders.jepsen@designit.com>
2015-02-08 00:39:42 +0100

    sound intro added

--- Assets/Scripts/StartGameGUI.cs
+++ Assets/Scripts/StartGameGUI.cs
@@ -7,17 +7,28 @@ public class StartGameGUI : VRGUI
 	public Texture instruction;
 	public GUISkin grannySkin;
 
+	private bool started = false;
+	
+	void Start() {
+
+	}
+
 	public override void OnVRGUI()
 	{
 		
 		GUI.skin = grannySkin;
-		GUILayout.BeginArea(new Rect(0, 0, Screen.width, Screen.height));
+		GUILayout.BeginArea (new Rect (0, 0, Screen.width, Screen.height));
 		
-		if (World.StartGame) {
-			Debug.Log("game started");
-			GUI.Box (new Rect ((float)((Screen.width-gameLogo.width)*0.5), 0, gameLogo.width, gameLogo.height), gameLogo);
-			GUI.Box(new Rect ((float)((Screen.width-instruction.width*0.5)*0.5), 0, (float)(instruction.width*0.5), (float)(instruction.height*0.5)), instruction);
+		if (!World.StartGame) {
+			Debug.Log ("game started");
+			GUI.Box (new Rect ((float)((Screen.width - gameLogo.width) * 0.5), 0, gameLogo.width, gameLogo.height), gameLogo);
+			GUI.Box (new Rect ((float)((Screen.width - instruction.width * 0.5) * 0.5), 0, (float)(instruction.width * 0.5), (float)(instruction.height * 0.5)), instruction);
+		} else {
+
+
 		}
 		GUILayout.EndArea();
 	}
+
+
 }
