526c550e346e864e04ed1af544262a6e0095853c
Anders Jepsen <anders.jepsen@designit.com>
2015-02-08 12:19:53 +0100

    please work now

--- Assets/Scripts/StartGameGUI.cs
+++ Assets/Scripts/StartGameGUI.cs
@@ -6,6 +6,7 @@ public class StartGameGUI : VRGUI
 	public Texture gameLogo;
 	public Texture instruction;
 	public GUISkin grannySkin;
+	public GUISkin tintSkin;
 
 	private bool started = false;
 	
@@ -18,11 +19,14 @@ public class StartGameGUI : VRGUI
 		
 		GUI.skin = grannySkin;
 		GUILayout.BeginArea (new Rect (0, 0, Screen.width, Screen.height));
+
+		string score = string.Format ("{0} {1}\n{2} {3}", "Best Combo:", World.BestCombo, "Total Score:", World.TotalScore); 
 		
 		if (!World.StartGame) {
 			
 			GUI.Box (new Rect (200, (float)((Screen.height - gameLogo.height) * 0.5), gameLogo.width, gameLogo.height), gameLogo);
 			GUI.Box (new Rect (400+gameLogo.width,(float)((Screen.height - instruction.height * 0.5) * 0.5), (float)(instruction.width * 0.5), (float)(instruction.height * 0.5)), instruction);
+			//GUI.Box (new Rect (0,0,Screen.width,Screen.height-300), score);
 		} else {
 
 
