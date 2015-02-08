bbaeb6cdc94d5e4a36cd791fb760dc46ecfa6bd4
Nicolai Egede Petersen <nicolaiegede@gmail.com>
2015-02-08 13:21:06 +0100

    misc edits

--- Assets/Scripts/StartGameGUI.cs
+++ Assets/Scripts/StartGameGUI.cs
@@ -24,9 +24,9 @@ public class StartGameGUI : VRGUI
 		
 		if (!World.StartGame) {
 			
-			GUI.Box (new Rect (200, (float)((Screen.height - gameLogo.height) * 0.5), gameLogo.width, gameLogo.height), gameLogo);
-			GUI.Box (new Rect (400+gameLogo.width,(float)((Screen.height - instruction.height * 0.5) * 0.5), (float)(instruction.width * 0.5), (float)(instruction.height * 0.5)), instruction);
-			//GUI.Box (new Rect (0,0,Screen.width,Screen.height-300), score);
+			GUI.Box (new Rect (350, (float)((Screen.height - gameLogo.height) * 0.5), gameLogo.width, gameLogo.height), gameLogo);
+			GUI.Box (new Rect (550+gameLogo.width,(float)((Screen.height - instruction.height * 0.5) * 0.5), (float)(instruction.width * 0.5), (float)(instruction.height * 0.5)), instruction);
+			GUI.Box (new Rect (0,0,Screen.width,Screen.height-300), score);
 		} else {
 
 
